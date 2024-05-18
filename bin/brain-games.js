7da4a74421b9a33d8bdc2ab1f502b2e52447ae96
SGD77 <102750788+SGD77@users.noreply.github.com>
2024-05-18 10:58:17 +0300

    fix brain-games shebang and permissions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import readlineSync from "readline-sync";
 const greeting = () => {
   const name = readlineSync.question("Welcome to the Brain Games!\nMay I have your name? ");
