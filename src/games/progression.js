2fe2bd4d57ee65dc7d6f383f7bd71240250aa3d5
SGD77 <102750788+SGD77@users.noreply.github.com>
2024-08-04 16:57:06 +0300

    fix: issue #8

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,14 +1,14 @@
 import start from '../index.js';
 import getRandomNumber from '../getRandom.js';
 
-const getRow = (startNum, step, length = 10) => {
+const generateProgression = (startNum, step, length = 10) => {
   const row = [];
   let rowNumber = startNum;
   for (let i = 0; i < length; i += 1) {
     rowNumber += step;
     row.push(rowNumber);
   }
-  return row.join(' ');
+  return row;
 };
 
 const hideAnswer = (row, index) => {
@@ -24,8 +24,8 @@ const condition = 'What number is missing in the progression?';
 const generateRound = () => {
   const startNum = getRandomNumber(0, 9);
   const step = getRandomNumber(1, 10);
-  const indexToGuess = getRandomNumber(0, getRow(startNum, step).split(' ').length);
-  return hideAnswer(getRow(startNum, step), indexToGuess);
+  const indexToGuess = getRandomNumber(0, generateProgression(startNum, step).split(' ').length);
+  return hideAnswer(generateProgression(startNum, step), indexToGuess);
 };
 
 export default () => {
